e9e1011a2150794f50f15bb7082cafcb54b09aa1
shweta230 <shweta230@gmail.com>
2017-02-01 22:08:32 +0530

    Resume-ref update

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 ////////////////SKILLS HIGHLIGHT ANIMATION//////////////////
 $(".talview").on("click",function(){    
-    $(".talview").attr('src','/shweta/shweta.github.io/img/tribal/t_talview3.png');
+    $(".talview").attr('src','img/tribal/t_talview3.png');
     $(".talview").removeClass("highlight"); 
     $(".talview").addClass("unhighlight");     
   });
