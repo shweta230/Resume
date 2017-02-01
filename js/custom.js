35126512d9e3125f4ac10124bc3f808df7980c0b
shweta230 <shweta230@gmail.com>
2017-02-01 22:00:12 +0530

    resume

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+/* Write here your custom javascript codes */
+$(document).ready(function() {    
+
+
+
+////////////////SKILLS HIGHLIGHT ANIMATION//////////////////
+$(".talview").on("click",function(){    
+    $(".talview").attr('src','/shweta/shweta.github.io/img/tribal/t_talview3.png');
+    $(".talview").removeClass("highlight"); 
+    $(".talview").addClass("unhighlight");     
+  });
+
+
+
+ $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+
+
+
+
+});
+
+////////////////SOFTWARE NAME ON HOVER//////////////////
+ document.addEventListener('DOMContentLoaded', function() {
+  $('.skill_img').on('mouseenter',function(){
+    $($(this).attr('src',$(this).data('target-src')));
+    });
+  $('.skill_img').on('mouseleave',function(){
+    $($(this).attr('src',$(this).data('target-src1')));     
+    });
+  });
+ ////////////////SOFTWARE NAME ON HOVER END//////////////////
